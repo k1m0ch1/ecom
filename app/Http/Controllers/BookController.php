b7d00dc176f964ec0a0b981c6f1b27f1e0d5b095
k1m0ch1 <yahya.kimochi@gmail.com>
2016-01-28 23:04:09 +0700

    Initiate Controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,7 +1,14 @@
 <?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use App\Models\Book;
+
 class BookController extends BaseController{
   	public function getIndex(){
     	$books = Book::all();
-    	return View::make('book_list')->with('books',$books);
+    	return view('book_list', compact('books'));
   	}
 }
